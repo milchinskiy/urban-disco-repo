06a722d28e98caaa399272df2f9507942444b487
Alexander Milchinskiy <milchinskiy@gmail.com>
2023-04-29 17:31:57 +0300

    update packages

--- prepare-aur-packages.sh
+++ prepare-aur-packages.sh
@@ -3,12 +3,15 @@
 PACKAGES="apple_cursor checkupdates+aur macchina-bin nordic-darker-theme whitesur-icon-theme xiccd"
 DIR="$(dirname ${0})/tmp"
 
+echo "Create dir ${DIR}"
 mkdir -p "$DIR"
 
+echo "download packages :${PACKAGES}"
 cd "$DIR" && yay -G $PACKAGES
 
 for f in *; do
     if [ -d "$f" ]; then
+        echo "making package ${f}"
         # Will not run if no directories are available
         cd "$f" && makepkg -f && cp ./*.pkg.tar.zst ../../x86_64/ && cd ..
     fi
@@ -18,4 +21,3 @@ cd ..
 rm -rf "$DIR"
 
 cd x86_64 && repo-add ./urban-disco-repo.db.tar.gz ./*.pkg.tar.zst
-
