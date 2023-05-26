959b1abc64a72edde194433587d4b4dffe3b76d1
Alexander Milchinskiy <milchinskiy@gmail.com>
2023-05-26 22:08:02 +0300

    update packages

--- prepare-aur-packages.sh
+++ prepare-aur-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="apple_cursor checkupdates+aur macchina-bin nordic-darker-theme whitesur-icon-theme xiccd"
+PACKAGES="apple_cursor checkupdates+aur macchina-bin nordic-darker-theme whitesur-icon-theme xiccd upd72020x-fw"
 DIR="$(dirname ${0})/tmp"
 
 echo "Create dir ${DIR}"
@@ -13,11 +13,11 @@ for f in *; do
     if [ -d "$f" ]; then
         echo "making package ${f}"
         # Will not run if no directories are available
-        cd "$f" && makepkg -f && cp ./*.pkg.tar.zst ../../x86_64/ && cd ..
+        cd "$f" && makepkg -f && cp -f ./*.pkg.tar.zst ../../x86_64/ && cd ..
     fi
 done
 
 cd ..
-rm -rf "$DIR"
+# rm -rf "$DIR"
 
 cd x86_64 && repo-add ./urban-disco-repo.db.tar.gz ./*.pkg.tar.zst
