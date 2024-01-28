f312f98a5df004a9c25111e6535a136c8fd2f79e
Alexander Milchinskiy <milchinskiy@gmail.com>
2024-01-28 12:56:45 +0200

    update packages

--- prepare-aur-packages.sh
+++ prepare-aur-packages.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-DIR="$(dirname "${0}")/tmp"
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+DIR="$SCRIPT_DIR/tmp"
 
 echo "Create dir ${DIR}"
 mkdir -p "$DIR"
 
-cd "$DIR" || exit
-packages=("apple_cursor" "albafetch" "xiccd" "xss-lock-session" "xfce-polkit")
+packages=("apple_cursor" "albafetch" "xiccd" "xss-lock-session" "xfce-polkit" "xstow")
 for f in "${packages[@]}"; do
+    cd "$DIR" || exit
     # git clone --branch "$f" --single-branch https://github.com/archlinux/aur.git "$f"
     curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/"$f".tar.gz
     tar -xvf "$f".tar.gz
     echo "making package ${f}"
     # Will not run if no directories are available
-    cd "$f" && makepkg -f -d && cp -f ./*.pkg.tar.zst ../../x86_64/ && cd ..
+    cd "$DIR/$f" && makepkg -f -d && mv ./*.pkg.tar.zst "$SCRIPT_DIR"/x86_64/
 done
 
-cd ..
+cd "$SCRIPT_DIR" || exit
 rm -rf "$DIR"
 
-cd x86_64 && repo-add ./urban-disco-repo.db.tar.gz ./*.pkg.tar.zst
+cd "$SCRIPT_DIR"/x86_64/ && ./rebuild.sh
