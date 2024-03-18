31ee34d738efaf22b923e5440601f32184bb89bc
Alexander Milchinskiy <milchinskiy@gmail.com>
2024-03-18 17:13:53 +0200

    add awesome-git

--- prepare-aur-packages.sh
+++ prepare-aur-packages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 DIR="$SCRIPT_DIR/tmp"
@@ -6,18 +6,18 @@ DIR="$SCRIPT_DIR/tmp"
 echo "Create dir ${DIR}"
 mkdir -p "$DIR"
 
-packages=("apple_cursor" "albafetch" "xiccd" "xss-lock-session" "xfce-polkit" "xstow" "wpaperd" "tofi" "moc-pulse")
+packages=("apple_cursor" "albafetch" "xiccd" "xss-lock-session" "xfce-polkit" "xstow" "moc-pulse" "awesome-git")
 for f in "${packages[@]}"; do
-    cd "$DIR" || exit
-    # git clone --branch "$f" --single-branch https://github.com/archlinux/aur.git "$f"
-    curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/"$f".tar.gz
-    tar -xvf "$f".tar.gz
-    echo "making package ${f}"
-    # Will not run if no directories are available
-    cd "$DIR/$f" && makepkg --rmdeps --syncdeps --noconfirm --clean && mv ./*.pkg.tar.zst "$SCRIPT_DIR"/x86_64/
+  cd "$DIR" || exit
+  # git clone --branch "$f" --single-branch https://github.com/archlinux/aur.git "$f"
+  curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/"$f".tar.gz
+  tar -xvf "$f".tar.gz
+  echo "making package ${f}"
+  # Will not run if no directories are available
+  cd "$DIR/$f" && makepkg --rmdeps --syncdeps --noconfirm --clean && mv ./*.pkg.tar.zst "$SCRIPT_DIR"/x86_64/
 done
 
 cd "$SCRIPT_DIR" || exit
 rm -rf "$DIR"
 
-cd "$SCRIPT_DIR"/x86_64/ && repo-add urban-disco-repo.db.tar.gz ./*.pkg.tar.zst
+cd "$SCRIPT_DIR"/x86_64/ && rm -rf "$SCRIPT_DIR"/x86_64/*-debug-* && repo-add urban-disco-repo.db.tar.gz ./*.pkg.tar.zst
